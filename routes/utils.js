b0dee159057ebbb0bcf7456c49da43e6368a914c
Rob Strasser <roestrasser@sbcglobal.net>
2021-10-18 11:12:08 -0700

    utils files added

---
+++ routes/utils.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const csrf = require('csurf');
+
+const csrfProtection = csrf({ cookie: true });
+const asyncHandler = handler => (req, res, next) => handler(req, res, next).catch(next);
+
+
+module.exports = {
+    csrfProtection,
+    asyncHandler
+}
