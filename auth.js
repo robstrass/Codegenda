f9eb668fbcc29717d2b3b0e8a0fcb1fffa805788
Rob Strasser <roestrasser@sbcglobal.net>
2021-10-18 11:06:07 -0700

    added auth functions

---
+++ auth.js
@@ -0,0 +1,44 @@
+const { db } = require('./config');
+
+const loginUser = (req, res, user) => {
+    req.session.auth = { userId: user.id }
+}
+
+const requireAuth = (req, res, next) => {
+    if (!res.locals.authenticated) {
+        return res.redirect('/users/login');
+    }
+    return next();
+}
+
+const restoreUser = async (req, res, next) => {
+    console.log(req.session)
+    if (req.session.auth) {
+        const { userId } = req.session.auth;
+        try {
+            const user = await db.User.findByPk(userId);
+            if (user) {
+                res.locals.authenticated = true;
+                res.locals.user = user;
+                next();
+            }
+        } catch (e) {
+            res.locals.authenticated = false;
+            next(e);
+        }
+    } else {
+        res.locals.authenticated = false;
+        next();
+    }
+}
+
+const logoutUser = (req, res) => {
+    delete req.session.auth;
+}
+
+module.exports = {
+    loginUser,
+    requireAuth,
+    restoreUser,
+    logoutUser
+}
