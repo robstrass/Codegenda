f53420a32c09470eff61f24891be5c0e829c57bf
josecantu_20@yahoo.com <josecantu_20@yahoo.com>
2021-10-18 16:21:24 -0500

    demoUserImplemented

---
+++ public/javascripts/users.js
@@ -0,0 +1,9 @@
+const { loginUser } = require("../../auth");
+
+window.onload('DOMContentLoaded', event => {
+    let demoUserButton = document.getElementById("demoButton");
+    demoUserButton.addEventListener('click', event => {
+        event.preventDefault();
+
+    })
+})
